a84e6a6ba69765670f1da776dbfdb8b0c2bcbb86
bogsan007-oss <bog.san007@gmail.com>
2026-07-11 14:45:21 +0200

    Delete dist/keys.js

--- dist/keys.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY_YOUTUBE = "твой_ключ";
-export const PLAYLIST_ID_1 = "твой_playlist_id";
-export const PLAYLIST_ID_2 = "твой_playlist_id";
-export const PLAYLIST_ID_3 = "твой_playlist_id";
