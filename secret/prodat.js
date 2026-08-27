95367f0e02c59c47e5d47d3905218a595e2fa7ad
bogsan007-oss <bog.san007@gmail.com>
2026-08-27 13:19:57 +0200

    Update prodat.js

--- secret/prodat.js
+++ secret/prodat.js
@@ -1,20 +1,21 @@
-{
-  title: "Настенный держатель ключей «Рыбацкая удача»",
-  img: "https://raw.githubusercontent.com/bogsan007-oss/img-fonts-css/main/secret/img/pod_kluci.webp",
+const prodat = [
+  {
+    title: "Настенный держатель ключей «Рыбацкая удача»",
+    img: "https://raw.githubusercontent.com/bogsan007-oss/img-fonts-css/main/secret/img/pod_kluci.webp",
     price: "EUR 7.40",
-  oldPrice: "EUR 16.08",
-  discount: "54%",
-  desc: "Винтажный настенный держатель ключей с удочками, катушками и блёснами. Плоский 2D‑принт, выполненный в стиле рыбацкой снасти. Идеально подходит для дома рыбака.",
-  link: "https://s.click.aliexpress.com/e/_c3uiMoUF"
-}
-{
-  title: "Мини‑пассатижи рыбака из нержавеющей стали",
-  img: "https://raw.githubusercontent.com/bogsan007-oss/img-fonts-css/main/secret/img/Ribak.webp",
+    oldPrice: "EUR 16.08",
+    discount: "54%",
+    desc: "Винтажный настенный держатель ключей с удочками, катушками и блёснами. Плоский 2D‑принт, выполненный в стиле рыбацкой снасти. Идеально подходит для дома рыбака.",
+    link: "https://s.click.aliexpress.com/e/_c3uiMoUF"
+  },
+
+  {
+    title: "Мини‑пассатижи рыбака из нержавеющей стали",
+    img: "https://raw.githubusercontent.com/bogsan007-oss/img-fonts-css/main/secret/img/Ribak.webp",
     price: "EUR 4.62",
-  oldPrice: "EUR 11.13",
-  discount: "58%",
-  desc: "Многофункциональные мини‑пассатижи из нержавеющей стали для рыбалки. Подходят для работы с приманками, леской и снастями. Компактные, удобные для переноски, идеальны для ловли басса и других видов рыбы.",
-  link: "https://s.click.aliexpress.com/e/_c3Wc6Q43"
-}
-
-
+    oldPrice: "EUR 11.13",
+    discount: "58%",
+    desc: "Многофункциональные мини‑пассатижи из нержавеющей стали для рыбалки. Подходят для работы с приманками, леской и снастями. Компактные, удобные для переноски, идеальны для ловли басса и других видов рыбы.",
+    link: "https://s.click.aliexpress.com/e/_c3Wc6Q43"
+  }
+];
