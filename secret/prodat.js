b107f5f542ba9ac778c0c1e4e1f948aa402128ab
bogsan007-oss <bog.san007@gmail.com>
2026-08-27 13:10:33 +0200

    Create prodat.js

---
+++ secret/prodat.js
@@ -0,0 +1,13 @@
+{
+  title: "Настенный держатель ключей «Рыбацкая удача»",
+  img: "https://raw.githubusercontent.com/bogsan007-oss/img-fonts-css/main/secret/img/pod_kluci.webp",
+  gallery: [
+    "IMAGE_UPLOADED_BY_USER"
+  ],
+  price: "EUR 7.40",
+  oldPrice: "EUR 16.08",
+  discount: "54%",
+  desc: "Винтажный настенный держатель ключей с удочками, катушками и блёснами. Плоский 2D‑принт, выполненный в стиле рыбацкой снасти. Идеально подходит для дома рыбака.",
+  link: "https://s.click.aliexpress.com/e/_c3uiMoUF"
+}
+
