78ba3bac2b791457baa65180c3e8af0c0f881236
bogsan007-oss <bog.san007@gmail.com>
2026-08-27 13:19:09 +0200

    Update prodat.js

--- secret/prodat.js
+++ secret/prodat.js
@@ -1,10 +1,7 @@
 {
   title: "Настенный держатель ключей «Рыбацкая удача»",
   img: "https://raw.githubusercontent.com/bogsan007-oss/img-fonts-css/main/secret/img/pod_kluci.webp",
-  gallery: [
-    "IMAGE_UPLOADED_BY_USER"
-  ],
-  price: "EUR 7.40",
+    price: "EUR 7.40",
   oldPrice: "EUR 16.08",
   discount: "54%",
   desc: "Винтажный настенный держатель ключей с удочками, катушками и блёснами. Плоский 2D‑принт, выполненный в стиле рыбацкой снасти. Идеально подходит для дома рыбака.",
@@ -13,10 +10,7 @@
 {
   title: "Мини‑пассатижи рыбака из нержавеющей стали",
   img: "https://raw.githubusercontent.com/bogsan007-oss/img-fonts-css/main/secret/img/Ribak.webp",
-  gallery: [
-    "IMAGE_UPLOADED_BY_USER"
-  ],
-  price: "EUR 4.62",
+    price: "EUR 4.62",
   oldPrice: "EUR 11.13",
   discount: "58%",
   desc: "Многофункциональные мини‑пассатижи из нержавеющей стали для рыбалки. Подходят для работы с приманками, леской и снастями. Компактные, удобные для переноски, идеальны для ловли басса и других видов рыбы.",
