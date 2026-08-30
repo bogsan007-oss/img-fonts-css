261ed3b150d15a99596c473e3d14b64c8c5eea6b
bogsan007-oss <bog.san007@gmail.com>
2026-08-30 16:10:38 +0200

    Create playlist.js

---
+++ secret/playlist.js
@@ -0,0 +1,13 @@
+// Отдельный файл с плейлистом треков
+window.myPlaylist = [
+    { title: "К Деду с пивом", src: "https://files.catbox.moe/2narmt.mp3" },
+	{ title: "Дворовый Воробей", src: "https://files.catbox.moe/wmwbx7.mp3" },
+    { title: "Жара сжигает всё", src: "https://files.catbox.moe/ybtx66.mp3" },
+    { title: "Вишнёвая серёжка", src: "https://files.catbox.moe/yzxy0t.mp3" },
+    { title: "Валиться из рук", src: "https://files.catbox.moe/pdtd7m.mp3" },
+    { title: "Загуляло лето, загуляло", src: "https://files.catbox.moe/2ufu06.mp3" },
+    { title: "Давление 200/120", src: "https://files.catbox.moe/0qlzvn.mp3" },
+    { title: "Стар капитан", src: "https://files.catbox.moe/3g0zuw.mp3" }
+    // Сюда можно добавлять новые песни через запятую:
+    // { title: "Новая песня", src: "ссылка_на_mp3" },
+];
