ec3c3c6fd1a705f0a27ad54cf5202c0eea3bb64a
bogsan007-oss <bog.san007@gmail.com>
2026-09-01 11:25:12 +0200

    Update playlist.js

--- secret/playlist.js
+++ secret/playlist.js
@@ -1,13 +1,22 @@
 // Отдельный файл с плейлистом треков
 window.myPlaylist = [
+    { title: "Я снова феникс", src: "https://files.catbox.moe/95b8pp.mp3" },
     { title: "К Деду с пивом", src: "https://files.catbox.moe/2narmt.mp3" },
-	{ title: "Дворовый Воробей", src: "https://files.catbox.moe/wmwbx7.mp3" },
+	{ title: "Дворовый Воробей", src: "https://files.catbox.moe/crsc0a.mp3" },
     { title: "Жара сжигает всё", src: "https://files.catbox.moe/ybtx66.mp3" },
     { title: "Вишнёвая серёжка", src: "https://files.catbox.moe/yzxy0t.mp3" },
     { title: "Валиться из рук", src: "https://files.catbox.moe/pdtd7m.mp3" },
     { title: "Загуляло лето, загуляло", src: "https://files.catbox.moe/2ufu06.mp3" },
     { title: "Давление 200/120", src: "https://files.catbox.moe/0qlzvn.mp3" },
-    { title: "Стар капитан", src: "https://files.catbox.moe/3g0zuw.mp3" }
+    { title: "Стар капитан", src: "https://files.catbox.moe/3g0zuw.mp3" },
+	{ title: "Курсом мимо Шапито", src: "https://files.catbox.moe/m4wl9u.mp3" },
+	{ title: "Не напишут стихов про меня", src: "https://files.catbox.moe/x2nvmj.mp3" },
+    { title: "О человечности", src: "https://files.catbox.moe/egltya.mp3" },
+    { title: "От безнаказаности", src: "https://files.catbox.moe/vvlbbr.mp3" },
+    { title: "Прости Гагарин", src: "https://files.catbox.moe/3ix925.mp3" },
+    { title: "Пыль на перстне", src: "https://files.catbox.moe/yi8egn.mp3" },
+    { title: "Размышление о лени", src: "https://files.catbox.moe/rbexlj.mp3" },
+    { title: "Сказать бы детям", src: "https://files.catbox.moe/mo4qym.mp3" }
     // Сюда можно добавлять новые песни через запятую:
     // { title: "Новая песня", src: "ссылка_на_mp3" },
 ];
